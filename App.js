485eb9ac2c1b2f51a9453dfa73309df09954064b
rahulmohonto <rahulmohonto1999@gmail.com>
2021-06-03 18:29:47 +0600

    branch creation and commit check

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi quasi provident ab atque eos beatae quos nesciunt vel ratione totam.</Text>
+      <Text>This is test</Text>
     </View>
   );
 }
